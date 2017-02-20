2f89ee85c1f47319358a72b1653a501815421d5a
John Damen <johndamen@wlan022138.mobiel.utwente.nl>
2017-02-20 10:49:09 +0100

    changed test to also plot on third axes

--- test.py
+++ test.py
@@ -11,6 +11,11 @@ ax.plot(x, np.sin(5*x), 'r-')
 ax = fig.add_subplot(2,2,1)
 ax.scatter(np.random.random(100), np.random.random(100), lw=0, color='k')
 ax = fig.add_subplot(2,2,2)
+ax.pcolormesh(
+    np.arange(20),
+    np.arange(20),
+    np.random.rand(20, 20),
+    cmap='inferno')
 
 adjust_axes(fig)
 plt.show()
