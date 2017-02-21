9454f2d07764622c0ce7a322a7083e57a53f3f77
John Damen <johndamen@Johns-MacBook-Pro.local>
2017-02-22 00:48:35 +0100

    extended existing anchor support on Axes objects. Ran into problems for Axes.stale which was called for Axes.set_anchor. stale not set now.

--- examples/example01.py
+++ examples/example01.py
@@ -5,11 +5,11 @@ fig = plt.figure()
 ax = fig.add_axes([.1, .1, .8, .8])
 p = plotutils.PositioningAxes.from_axes(fig, ax, anchor='c')
 
-p.set_anchor_point('c')
+p.set_anchor_point('C')
 print(p.x, p.y)
 
-p.set_anchor_point('ll')
+p.set_anchor_point('SW')
 print(p.x, p.y)
 
-p.set_anchor_point('ur')
+p.set_anchor_point('NE')
 print(p.x, p.y)
